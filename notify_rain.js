8298b75cb3bd8d6d305999573f392ef23099d5c3
ttytb <teu@tmhn.net>
2020-08-08 11:43:23 +0900

    インターバル毎に繰り返す

--- notify_rain.js
+++ notify_rain.js
@@ -28,43 +28,47 @@ const jsonget = function (url) {
   return rp(options);
 };
 
-jsonget(YAHOO_URL)
-  .then((json) => {
-    const data = json["Feature"][0]["Property"]["WeatherList"]["Weather"];
-    let maxRain = 0;
-    for (let index = 0; index < COUNT; index++) {
-      const element = data[index];
-      const rain = element["Rainfall"];
-      const dt = moment(element["Date"], "YYYYMMDDHHmmss");
-      console.log(`${dt.format("HH:mm")} ${rain}mm`);
-      if (rain > maxRain) {
-        maxRain = rain;
+function intervalFunc() {
+  jsonget(YAHOO_URL)
+    .then((json) => {
+      const data = json["Feature"][0]["Property"]["WeatherList"]["Weather"];
+      let maxRain = 0;
+      for (let index = 0; index < COUNT; index++) {
+        const element = data[index];
+        const rain = element["Rainfall"];
+        const dt = moment(element["Date"], "YYYYMMDDHHmmss");
+        console.log(`${dt.format("HH:mm")} ${rain}mm`);
+        if (rain > maxRain) {
+          maxRain = rain;
+        }
       }
-    }
-    let pastRain = 0;
-    try {
-      pastRain = fs.readFileSync(STATE_FILE, "utf8");
-    } catch (error) {
+      let pastRain = 0;
       try {
-        fs.writeFileSync(STATE_FILE, 0);
+        pastRain = fs.readFileSync(STATE_FILE, "utf8");
       } catch (error) {
-        console.log(error);
+        try {
+          fs.writeFileSync(STATE_FILE, 0);
+        } catch (error) {
+          console.log(error);
+        }
       }
-    }
-    let msg = null;
-    if (maxRain >= THRESHOLD && pastRain < THRESHOLD) {
-      msg = `もうすぐ、強い雨が降り出します。雨量は、最大${maxRain}ミリです。`;
-    } else if (maxRain > 1 && pastRain <= 1) {
-      msg = `もうすぐ、雨が降り出します。雨量は、最大${maxRain}ミリです。`;
-    }
-    if (!isNull(msg)) {
-      ghn.device(cf.config.device, LANG);
-      ghn.ip(cf.config.ip);
-      ghn.accent(LANG);
-      ghn.notify(msg, (res) => console.log(`said ${msg}`));
-    }
-    fs.writeFileSync(STATE_FILE, maxRain);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+      let msg = null;
+      if (maxRain >= THRESHOLD && pastRain < THRESHOLD) {
+        msg = `もうすぐ、強い雨が降り出します。雨量は、最大${maxRain}ミリです。`;
+      } else if (maxRain > 1 && pastRain <= 1) {
+        msg = `もうすぐ、雨が降り出します。雨量は、最大${maxRain}ミリです。`;
+      }
+      if (!isNull(msg)) {
+        ghn.device(cf.config.device, LANG);
+        ghn.ip(cf.config.ip);
+        ghn.accent(LANG);
+        ghn.notify(msg, (res) => console.log(`said ${msg}`));
+      }
+      fs.writeFileSync(STATE_FILE, maxRain);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+setInterval(intervalFunc, cf.config.eew_interval * 60000);
